8c99bc340dce0dc1abd80464b6d274f472d772bc
wsderte <31801595+wsderte@users.noreply.github.com>
2022-04-08 14:51:29 +0300

    fix styles, small changes

--- src/components/course-assets/course-assets.js
+++ src/components/course-assets/course-assets.js
@@ -1,17 +1,16 @@
-import React from "react";
-import "./course-assets.css";
+import React from 'react'
+import './course-assets.css'
 
 export const CourseAssets = ({ text, icon, color }) => {
-  if (!color) {
-    color = "rgb(126, 200, 56)";
-  }
-
-  return (
-    <div className="assets-flex">
-      <div className="assets-icon" style={{ color: color }}>
-        {icon}
-      </div>
-      <div className="assets-text">{text}</div>
-    </div>
-  );
-};
+    return (
+        <div className="assets-flex">
+            <div
+                className="assets-icon"
+                style={{ color: color ? color : 'rgb(126, 200, 56)' }}
+            >
+                {icon}
+            </div>
+            <div className="assets-text">{text}</div>
+        </div>
+    )
+}
